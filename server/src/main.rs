d71a949be9889ce15c940e8c44d60bdb76f41673
Mario Fleischhacker <mario.fleischhacker@gmail.com>
2023-09-21 06:49:32 +0200

    chore: add server scaffold

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,34 @@
-fn main() {
-    println!("Hello, world! from server");
-    let serve = env!("CLIENT_DIST");
-    println!("going to serve content from {serve}");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use axum::routing::get;
+    use axum::Router;
+    use std::net::SocketAddr;
+
+    setup_tracing()?;
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+pub(crate) fn setup_tracing() -> Result<(), Box<dyn std::error::Error>> {
+    use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .try_init()?;
+
+    Ok(())
+}
+
+async fn root() -> &'static str {
+    const SERVE: &str = env!("CLIENT_DIST");
+    const_format::concatcp!("Hello ", SERVE)
 }

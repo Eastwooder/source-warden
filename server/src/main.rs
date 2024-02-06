799e0916b52e8b8f492f4c30cc278edd9a9d9014
Mario Fleischhacker <mario.fleischhacker@gmail.com>
2024-02-06 19:41:52 +0900

    fix: listen from all addresses, not only localhost

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,7 +8,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let app = Router::new().route("/", get(root));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())

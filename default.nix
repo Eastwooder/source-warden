439f91daa99ddc591cae0e1de7a125acdf0b7e5a
Mario Fleischhacker <mario.fleischhacker@gmail.com>
2024-02-09 00:02:58 +0900

    fix: ensure default.nix and shell.nix use the same version of compat as defined in flake.lock

--- default.nix
+++ default.nix
@@ -1,4 +1,11 @@
 # Generated with 'nix flake init --template templates#compat' to satisfy VS Codium's Nix environment selector plugin
-(import (fetchTarball https://github.com/edolstra/flake-compat/archive/master.tar.gz) {
-  src = builtins.fetchGit ./.;
-}).defaultNix
+(import
+  (
+    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+      sha256 = lock.nodes.flake-compat.locked.narHash;
+    }
+  )
+  { src = builtins.fetchGit ./.; }
+).defaultNix
